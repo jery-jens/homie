71164283df91aed9e1793aca52d47d2b0db01a58
Jens Deryckere <97600411+jery-jens@users.noreply.github.com>
2022-06-21 13:06:19 +0200

    Questions event

---
+++ js/index.js
@@ -0,0 +1,34 @@
+let activeQuestion = 0;
+
+const questions = document.querySelectorAll(".form-question");
+const next = document.querySelector(".next");
+const prev = document.querySelector(".prev");
+const pagination = document.querySelectorAll(".step");
+console.log(questions, next, prev, pagination)
+
+const changeQuestion = (activeQuestion) => {
+    console.log(activeQuestion)
+    if ((questions.length - 1) < activeQuestion) {
+        questions.forEach((question, index) => {
+            if (index === activeQuestion) {
+                question.classList.add("active");
+                question.classList.remove("hidden");
+                pagination[activeQuestion].classList.add("active");
+            } else {
+                question.classList.remove("active");
+                question.classList.add("hidden");
+                pagination[activeQuestion].classList.remove("active");
+            };
+        });
+    } else {
+        // Submit the form
+    }
+};
+
+next.addEventListener("click", () => {
+    changeQuestion(activeQuestion + 1);
+});
+
+prev.addEventListener("click", () => {
+    changeQuestion(activeQuestion - 1);
+});
