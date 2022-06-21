b38694f457b7466b57c6489153d3ce9638eb8ca5
Jens Deryckere <97600411+jery-jens@users.noreply.github.com>
2022-06-21 14:16:31 +0200

    Update index.js

--- js/index.js
+++ js/index.js
@@ -5,11 +5,16 @@ document.addEventListener("DOMContentLoaded", () => {
     const next = document.querySelector(".form-step-nav.next");
     const prev = document.querySelector(".form-step-nav.prev");
     const pagination = document.querySelectorAll(".step");
-    console.log(questions, next, prev, pagination)
     
     const changeQuestion = (activeQuestion) => {
         const max = questions.length - 1;
-        console.log(max, activeQuestion)
+
+        if (activeQuestion > 0) {
+            prev.style.display = "block";
+        } else {
+            prev.style.display = "none";
+        };
+
         if (max <= activeQuestion) {
             questions.forEach((question, index) => {
                 if (index === activeQuestion) {
