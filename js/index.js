2834a6e0cea3e3cb11bd11abf352fbcf46e2a84d
Jens Deryckere <97600411+jery-jens@users.noreply.github.com>
2022-06-27 22:32:27 +0200

    Add results

--- js/index.js
+++ js/index.js
@@ -251,9 +251,31 @@ document.addEventListener("DOMContentLoaded", () => {
     //  * Calculate and show TCO results
     //  */
 
-    //  const tcoUrl = new URL(window.location.href);
+     const tcoUrl = new URL(window.location.href);
 
-     
+     const credit = tcoUrl.searchParams.get("credit") ?? 0;
+     const onPurchase = tcoUrl.searchParams.get("on_purchase") ?? 0;
+     const afterPurchase = tcoUrl.searchParams.get("after_purchase") ?? 0;
+     const yearly = tcoUrl.searchParams.get("yearly") ?? 0;
+
+     const totalOut = document.querySelector(".total-out");
+     const totalPurchase = document.querySelector(".total-purchase");
+     const totalExpect = document.querySelector(".total-expect");
+     const totalCredit = document.querySelector(".total-credit");
+     const totalYearly = document.querySelectorAll(".total-yearly-out");
+
+     if (totalOut) {
+        totalOut.innerHTML = "€" + Math.round(credit + onPurchase + afterPurchase);
+        totalExpect.innerHTML = "€" + afterPurchase;
+        totalPurchase.innerHTML = "€" + onPurchase;
+        totalCredit.innerHTML = "€" + credit;
+     };
+
+     if (yearly) {
+        totalYearly.forEach((item) => {
+            item.innerHTML = "€" + yearly;
+        });
+     };
 
     /**
      * Tool selector
