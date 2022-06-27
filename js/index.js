d80e9864e2bf55bc63e70789ce967ec5a30423cb
Jens Deryckere <97600411+jery-jens@users.noreply.github.com>
2022-06-27 11:31:58 +0200

    Update index.js

--- js/index.js
+++ js/index.js
@@ -5,8 +5,39 @@ document.addEventListener("DOMContentLoaded", () => {
 
     window.addEventListener("scroll", () => {
         const header = document.querySelector(".main-menu");
-        const top  = window.pageYOffset || document.documentElement.scrollTop;
+        const top = window.pageYOffset || document.documentElement.scrollTop;
         top >= 65 ? header.classList.add("active") : header.classList.remove("active");
+    });
+
+    /**
+     * Switches
+     */
+
+    const switches = document.querySelectorAll(".switch-checkbox-field");
+    console.log(switches)
+
+    switches.forEach((item, index) => {
+        const wrapper = item.parentElement.parentElement;
+        const circle = wrapper.children[0];
+        const label = wrapper.parentElement.children[1];
+
+        console.log(wrapper, circle, label);
+
+        item.addEventListener("change", () => {
+            console.log(item.checked);
+
+            if (item.checked) {
+                wrapper.classList.add("active");
+                circle.classList.add("active");
+                label.classList.add("active");
+                label.innerHTML = "Ja";
+            } else {
+                wrapper.classList.remove("active");
+                circle.classList.remove("active");
+                label.classList.remove("active");
+                label.innerHTML = "Nee";
+            };
+        });
     });
 
     /**
@@ -30,19 +61,19 @@ document.addEventListener("DOMContentLoaded", () => {
     if (questions) {
         const changeQuestion = (activeQuestion) => {
             const max = questions.length - 1;
-    
+
             if (activeQuestion > 0) {
                 prev.style.display = "block";
             } else {
                 prev.style.display = "none";
             };
-    
+
             if (activeQuestion === max) {
                 next.style.display = "none";
             } else {
                 next.style.display = "block";
             };
-    
+
             questions.forEach((question, index) => {
                 if (activeQuestion === index) {
                     question.classList.add("active");
@@ -55,29 +86,15 @@ document.addEventListener("DOMContentLoaded", () => {
                 };
             });
         };
-        
+
         next.addEventListener("click", () => {
             activeQuestion = activeQuestion + 1;
             changeQuestion(activeQuestion);
         });
-        
+
         prev.addEventListener("click", () => {
             activeQuestion = activeQuestion - 1;
             changeQuestion(activeQuestion);
         });
     };
-
-    /**
-     * Switches
-     */
-
-    const switches = document.querySelectorAll(".switch-checkbox-field");
-
-    switches.forEach((item, index) => {
-        const wrapper = item.parentElement.parentElement;
-        const circle = wrapper.children[0];
-        const label = wrapper.parentElement.children[1];
-
-        console.log(wrapper, circle, label)
-    });
 });
