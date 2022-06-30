9a313c4ad7536dd65ac6eb43cf459d448c0842b5
Jens Deryckere <97600411+jery-jens@users.noreply.github.com>
2022-06-30 21:32:12 +0200

    Update questions.js

--- js/tco/questions.js
+++ js/tco/questions.js
@@ -82,7 +82,7 @@ document.addEventListener("DOMContentLoaded", () => {
 
   const calcYearlyHypo = (perc, amount, months) => {
     const oneMonth = (1 + perc) ** (1 / 12) - 1;
-    console.log(oneMonth.toFixed(4));
+    console.log(oneMonth.toFixed(4), -Math.abs(months));
     const costOneMonth = Number(amount) * oneMonth.toFixed(4) / (1 - (1 + perc)**(-Math.abs(months)));
     return (costOneMonth * 12).toFixed(2);
   };
