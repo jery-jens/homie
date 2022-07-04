6bd50738e534117abbe12846a261b2fc9fde253a
Jens Deryckere <97600411+jery-jens@users.noreply.github.com>
2022-07-04 20:30:18 +0200

    Update index

--- js/index.js
+++ js/index.js
@@ -15,59 +15,16 @@ document.addEventListener("DOMContentLoaded", () => {
 
     const selector = document.querySelector(".btn-tool-select");
 
+    /**
+     * Info show
+     */
 
-    /** 
-     * Questions 
-     * */
+    const infoBtn = document.querySelector(".btn-info");
+    const infoPopup = document.querySelector(".popup-tool-info");
 
-    let activeQuestion = 0;
-
-    const questions = document.querySelectorAll(".form-question");
-    const next = document.querySelector(".form-step-nav.next");
-    const prev = document.querySelector(".form-step-nav.prev");
-    const pagination = document.querySelectorAll(".step");
-
-    if (questions) {
-        const changeQuestion = (activeQuestion) => {
-            const max = questions.length - 1;
-
-            if (activeQuestion > 0) {
-                prev.style.display = "block";
-            } else {
-                prev.style.display = "none";
-            };
-
-            if (activeQuestion === max) {
-                next.style.display = "none";
-            } else {
-                next.style.display = "block";
-            };
-
-            questions.forEach((question, index) => {
-                if (activeQuestion === index) {
-                    question.classList.add("active");
-                    question.classList.remove("hidden");
-                    pagination[index].classList.add("active");
-                } else {
-                    question.classList.remove("active");
-                    question.classList.add("hidden");
-                    pagination[index].classList.remove("active");
-                };
-            });
-        };
-
-        if (next) {
-            next.addEventListener("click", () => {
-                activeQuestion = activeQuestion + 1;
-                changeQuestion(activeQuestion);
-            });    
-        };
-
-        if (prev) {
-            prev.addEventListener("click", () => {
-                activeQuestion = activeQuestion - 1;
-                changeQuestion(activeQuestion);
-            });
-        };
+    if (infoBtn && infoPopup) {
+        infoBtn.addEventListener("click", () => {
+            console.log("jaaaa!");
+        });
     };
 });
