e75f39fa93e759688977e3959ec67fa193987da4
Jens Deryckere <97600411+jery-jens@users.noreply.github.com>
2022-07-04 14:21:41 +0200

    Create real-estate.js

---
+++ js/buying-plan/real-estate.js
@@ -0,0 +1,13 @@
+document.addEventListener("DOMContentLoaded", () => {
+    // Sections
+    const resultSection = document.querySelector(".student-section");
+
+    // Type of estate
+    const house = document.getElementById("house");
+    const appartment = document.getElementById("appartment");
+    const assistantHouse = document.getElementById("assistanthouse");
+    const studentHouse = document.getElementById("studenthouse");
+    const vacationHouse = document.getElementById("vacationhouse");
+
+    console.log(house, appartment, assistantHouse, studentHouse, vacationHouse);
+});
