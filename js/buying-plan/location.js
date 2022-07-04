547d7eed5a1b9c88261e1db908944c339c792155
Jens Deryckere <97600411+jery-jens@users.noreply.github.com>
2022-07-04 19:00:14 +0200

    Update location.js

--- js/buying-plan/location.js
+++ js/buying-plan/location.js
@@ -4,13 +4,17 @@ document.addEventListener("DOMContentLoaded", () => {
     const citiesField = document.getElementById("cities");
 
     // Check if session
-    
+    const provinceStorage = sessionStorage.getItem("bp_province") ?? null;
+    const citiesStorage = sessionStorage.getItem("bp_cities") ?? null;
+
+    if (provinceStorage) provinceField.value = provinceStorage;
+    if (citiesStorage) citiesStorage.value = citiesStorage;
 
     provinceField.addEventListener("input", () => {
-
+        sessionStorage.setItem("bp_province", provinceField.value);
     });
 
     citiesField.addEventListener("input", () => {
-
+        sessionStorage.setItem("bp_cities", provinceField.value);
     });
 });
