c8752a5f2091ee01ac8ed1a0d487a6e45b6c3c49
Jens Deryckere <97600411+jery-jens@users.noreply.github.com>
2022-07-04 17:41:51 +0200

    Create location.js

---
+++ js/buying-plan/location.js
@@ -0,0 +1,16 @@
+document.addEventListener("DOMContentLoaded", () => {
+    // Fields
+    const provinceField = document.getElementById("province");
+    const citiesField = document.getElementById("cities");
+
+    // Check if session
+    
+
+    provinceField.addEventListener("input", () => {
+
+    });
+
+    citiesField.addEventListener("input", () => {
+
+    });
+});
