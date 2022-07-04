5bc98c888c4f573698b087805ee1f88197154e9c
Jens Deryckere <97600411+jery-jens@users.noreply.github.com>
2022-07-04 19:12:30 +0200

    Update real-estate.js

--- js/buying-plan/real-estate.js
+++ js/buying-plan/real-estate.js
@@ -10,9 +10,9 @@ document.addEventListener("DOMContentLoaded", () => {
     const oldTips = document.querySelector(".when-both");
 
     // Arrays
-    const selectedEstates = [];
-    const selectedPeople = [];
-    const selectedStates = [];
+    let selectedEstates = [];
+    let selectedPeople = [];
+    let selectedStates = [];
 
     // Type of estate
     const house = document.getElementById("house");
@@ -30,7 +30,12 @@ document.addEventListener("DOMContentLoaded", () => {
     const seniors = document.getElementById("seniors");
 
     // Variables
-    const budget = Number(sessionStorage.getItem("budget")) ?? 0;
+    const budget = Number(sessionStorage.getItem("bp_values").budget) ?? 0;
+
+    // Session
+    const estatesStorage = JSON.parse(sessionStorage.get("bp_estates")) ?? null;
+    const peopleStorage = JSON.parse(sessionStorage.get("bp_people")) ?? null;
+    const statesStorage = JSON.parse(sessionStorage.get("bp_states")) ?? null;
 
     // Change selected
     const radioActionEstate = (item) => {
@@ -196,4 +201,67 @@ document.addEventListener("DOMContentLoaded", () => {
     families.addEventListener("click", () => {
         radioActionPeople(families);
     });
+
+    // Watch session
+    if (estatesStorage) {
+        selectedEstates = estatesStorage;
+
+        selectedEstates.forEach((estate) => {
+            const selectedEstate = document.getElementById(estate);
+            selectedEstate.children[0].classList.add("active");
+        });
+
+        if (selectedEstates.length) {
+            resultSection.style.display = "block";
+            requirementsSection.style.display = "block";
+            efficientSections.forEach((section) => {
+                section.style.display = "block";
+            });
+
+            // Calculate reachability
+            let reachability = true;
+
+            selectedEstates.forEach((estate) => {
+                if (reachability) {
+                    if (estate === "house") {
+                        budget >= 200000 ? reachability = true : reachability = false;
+                    };
+
+                    if (estate === "appartment") {
+                        budget >= 100000 ? reachability = true : reachability = false;
+                    };
+
+                    if (estate === "studenthouse") {
+                        budget >= 80000 ? reachability = true : reachability = false;
+                    };
+
+                    if (estate === "assistanthouse") {
+                        budget >= 150000 ? reachability = true : reachability = false;
+                    };
+
+                    if (estate === "vacationhouse") {
+                        budget >= 90000 ? reachability = true : reachability = false;
+                    };
+                };
+            });
+
+            if (!reachability) {
+                resultTitle.innerHTML = "niet haalbaar";
+                resultTitle.classList.add("not-green");
+                reachableTips.style.display = "none";
+                noReachableTips.style.display = "flex";
+            } else {
+                resultTitle.innerHTML = "haalbaar";
+                resultTitle.classList.remove("not-green");
+                reachableTips.style.display = "flex";
+                noReachableTips.style.display = "none";
+            };
+        } else {
+            resultSection.style.display = "none";
+            requirementsSection.style.display = "none";
+            efficientSections.forEach((section) => {
+                section.style.display = "none";
+            });
+        };
+    };
 });
