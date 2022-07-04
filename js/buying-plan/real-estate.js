05412219572cf92c98538bbb685dc0c7dafb5520
Jens Deryckere <97600411+jery-jens@users.noreply.github.com>
2022-07-04 15:33:51 +0200

    Update real-estate.js

--- js/buying-plan/real-estate.js
+++ js/buying-plan/real-estate.js
@@ -2,6 +2,9 @@ document.addEventListener("DOMContentLoaded", () => {
     // Sections
     const resultSection = document.querySelector(".student-section");
 
+    // Arrays
+    const selectedEstates = [];
+
     // Type of estate
     const house = document.getElementById("house");
     const appartment = document.getElementById("appartment");
@@ -9,7 +12,25 @@ document.addEventListener("DOMContentLoaded", () => {
     const studentHouse = document.getElementById("studenthouse");
     const vacationHouse = document.getElementById("vacationhouse");
 
+    // Change selected
+    const radioAction = (item) => {
+        const btn = item.children[0];
+        const wrapper = item.parentElement;
+
+        if (selectedEstates.includes(item.id)) {
+            selectedEstates.forEach((estate, index) => {
+                if (estate === item.id) {
+                    selectedEstates.splice(index, 1);
+                    btn.classList.remove("active");
+                };
+            });
+        } else {
+            selectedEstates.push(item.id);
+            btn.classList.add("active");
+        };
+    };
+
     house.addEventListener("click", () => {
-        console.log(house.children[0]);
+        radioAction(house);
     });
 });
