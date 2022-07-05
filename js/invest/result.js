4bc43d181ba96902dbd39a178d650f20dc76e5e7
Jens Deryckere <97600411+jery-jens@users.noreply.github.com>
2022-07-05 17:12:16 +0200

    Update result.js

--- js/invest/result.js
+++ js/invest/result.js
@@ -18,6 +18,9 @@ document.addEventListener("DOMContentLoaded", () => {
     const carLoans = Number(url.searchParams.get("car_loans") ?? 0);
     const alimony = Number(url.searchParams.get("alimony") ?? 0);
     const monthlyPayment = (netIncome - (otherLoans + carLoans + alimony)) / 100 * 40;
+    const financialState = url.searchParams.get("financial") ?? "safe";
+    const hasInvestments = url.searchParams.get("investments") ?? "yes";
+    const investmentsConcern = url.searchParams.get("investments_concern") ?? "yes";
 
     // Actions
     const loanEvent = () => {
@@ -27,14 +30,25 @@ document.addEventListener("DOMContentLoaded", () => {
 
         // Show values
         document.querySelectorAll(".choosen-term").forEach((element) => {
-            element.innerHTML = loanTerm.value;
+            element.innerHTML = loanTerm.value + " jaar";
         });
         document.querySelector(".loan-amount").innerHTML = "€ " + averageLoanAmount;
         document.querySelector(".monthly-loan-payment").innerHTML = "€ " + monthlyPayment;
     };
 
+    const riskToleranceEvent = () => {
+        let riskTolerance = "low";
+
+        if (financialState === "danger" && hasInvestments === "no" && investmentsConcern === "high") riskTolerance = "low";
+        if (financialState === "hard" && investmentsConcern === "average") riskTolerance = "medium";
+        if (financialState === "safe" && hasInvestments === "yes" && investmentsConcern === "none") riskTolerance = "high";
+
+        document.querySelector(".risk-tolerance").innerHTML = riskTolerance === "low" ? "Lage risicotolerantie" : riskTolerance === "medium" ? "Gemiddelde risicotolerantie" : "Hoge risicotolerantie";
+    };
+
     // Default actions
     loanEvent();
+    riskToleranceEvent();
 
     // Field listeners
     loanTerm.addEventListener("input", () => {
