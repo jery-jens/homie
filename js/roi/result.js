0dbac308c469672ae97ed4d91a6081415d75299c
Jens Deryckere <97600411+jery-jens@users.noreply.github.com>
2022-07-06 22:37:38 +0200

    Update result.js

--- js/roi/result.js
+++ js/roi/result.js
@@ -5,14 +5,14 @@ document.addEventListener("DOMContentLoaded", () => {
 
   const roiUrl = new URL(window.location.href);
 
-  const purchasePrice = roiUrl.searchParams.get("purchase_price") ?? 0;
-  const rentPrice = roiUrl.searchParams.get("rent_price") ?? 0;
+  const purchasePrice = Number(roiUrl.searchParams.get("purchase_price")) ?? 0;
+  const rentPrice = Number(roiUrl.searchParams.get("rent_price")) ?? 0;
   const region = roiUrl.searchParams.get("region") ?? 0;
-  const oneTimeCosts = roiUrl.searchParams.get("one_time_costs") ?? 0;
-  const amountTimeEmpty = roiUrl.searchParams.get("amount_time_empty") ?? 0;
-  const yearlyCosts = roiUrl.searchParams.get("yearly_costs") ?? 0;
-  const propertyTax = roiUrl.searchParams.get("property_tax") ?? 0;
-  const yearlyAmountInsurances = roiUrl.searchParams.get("yearly_amount_insurances") ?? 0;
+  const oneTimeCosts = Number(roiUrl.searchParams.get("one_time_costs")) ?? 0;
+  const amountTimeEmpty = Number(roiUrl.searchParams.get("amount_time_empty")) ?? 0;
+  const yearlyCosts = Number(roiUrl.searchParams.get("yearly_costs")) ?? 0;
+  const propertyTax = Number(roiUrl.searchParams.get("property_tax")) ?? 0;
+  const yearlyAmountInsurances = Number(roiUrl.searchParams.get("yearly_amount_insurances")) ?? 0;
   const brutRent = roiUrl.searchParams.get("bruto_rent") ?? 0;
   const netRent = roiUrl.searchParams.get("netto_rent") ?? 0;
   const rentYear = rentPrice * 12;
@@ -23,6 +23,7 @@ document.addEventListener("DOMContentLoaded", () => {
   const testNet = ((rentYear - (amountTimeEmpty * rentPrice) - propertyTax - yearlyCosts - yearlyAmountInsurances) / (purchasePrice + oneTimeCosts)) * 100;
 
   console.log(rentYear, amountTimeEmpty, rentPrice, propertyTax, yearlyCosts, yearlyAmountInsurances, purchasePrice, oneTimeCosts);
+  console.log(testNet);
 
   net.innerHTML = "€ " + netRent;
   brut.innerHTML = "€ " + brutRent;
