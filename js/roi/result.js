934a2aab9dcf69fa5566dc4a09074a6af54a9fcf
Jens Deryckere <97600411+jery-jens@users.noreply.github.com>
2022-07-06 22:26:41 +0200

    Update result.js

--- js/roi/result.js
+++ js/roi/result.js
@@ -19,7 +19,7 @@ document.addEventListener("DOMContentLoaded", () => {
   const net = document.querySelector(".net");
   const brut = document.querySelector(".brut");
 
-  const testNet = (((rentPrice * 12) - (amountTimeEmpty * rentPrice) - yearlyCosts - propertyTax - yearlyAmountInsurances) / (purchasePrice + oneTimeCosts)) * 100;
+  const testNet = (((rentPrice * 12) - (amountTimeEmpty * rentPrice) - yearlyCosts - propertyTax - yearlyAmountInsurances) / (purchasePrice + (oneTimeCosts * (12.5/100)))) * 100;
   console.log(testNet);
 
   net.innerHTML = "€ " + netRent;
